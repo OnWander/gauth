8e4420e3de885b443bb721237fbdad2ad16b4ebc
Whitney Zoller <whitneyrzoller@gmail.com>
2013-07-31 08:14:17 -0400

    implementing canonicalRepresentation and Authentication

--- hmac.go
+++ hmac.go
@@ -1,23 +1,111 @@
 package gauth
 
 import (
-  "net/http"
   "bytes"
+  "crypto/hmac"
+  "crypto/sha256"
+  "errors"
+  "net/http"
+  "os"
   "strings"
+  "log"
+  "sort"
+  "net/url"
 )
 
-func CanonicalRep(req *http.Request) (rep string, err error) {
-  err = req.ParseForm()
+const (
+  defaultMaxMemory = 32 << 20 // 32 MB
+  authHeader = "Authentication"
+  xHMACPrefix = "Hmac"
+  xHMACDate = "X-Hmac-Date"
+  xHMACNonce = "X-Hmac-Nonce"
+)
+
+func Authenticate(req *http.Request) (err error) {
+  authHeader := req.Header.Get(authHeader)
+  if len(authHeader) == 0 {
+    return errors.New("Missing Authentication header")
+  }
+
+  authTkns := strings.Split(authHeader, " ")
+  if len(authTkns) != 2 {
+    return errors.New("Malformed auth-tokens")
+  }
+
+  //apiKey := authTkns[0]
+  signature := []byte(authTkns[1])
+
+  canonicalRep, err := canonicalRep(req)
+  if err != nil {
+    return err
+  }
+  sharedSecret := []byte(os.Getenv("SHARED_SECRET"))
+  expectedMAC := computeSignature([]byte(canonicalRep), sharedSecret)
+
+  if !hmac.Equal(signature, expectedMAC){
+    return errors.New("Error authenticating")
+  }
+
+  return nil
+}
+
+func checkMAC(message, messageMAC, key []byte) bool {
+  mac := hmac.New(sha256.New, key)
+  mac.Write(message)
+  expectedMAC := mac.Sum(nil)
+  return hmac.Equal(messageMAC, expectedMAC)
+}
+
+func computeSignature(message, sharedSecret []byte) []byte {
+  mac := hmac.New(sha256.New, sharedSecret)
+  mac.Write(message)
+  return mac.Sum(nil)
+}
+
+func canonicalRep(req *http.Request) (rep string, err error) {
+  err = req.ParseMultipartForm(defaultMaxMemory)
   if err != nil {
     return rep, err
   }
+  log.Printf("--- req.Form: %s", req.Form)
   var repBuff bytes.Buffer
 
-  // original URI
-  repBuff.WriteString(req.RequestURI)
   // HTTP verb (GET, POST,...) uppercased
-  repBuff.WriteString(strings.ToUpper(req.Method))
+  repBuff.WriteString(strings.ToUpper(req.Method+"\n"))
+  // original URI
+  repBuff.WriteString(req.RequestURI+"\n")
+
+  // original headers
+  headers := req.Header
+  log.Printf("--- req.Header: %s", headers)
+  if hmacDate := headers[xHMACDate]; len(hmacDate) > 0 {
+    repBuff.WriteString("date:"+hmacDate[0]+"\n")
+  } else {
+    repBuff.WriteString("date:\n")
+  }
+  if hmacNonce := headers[xHMACNonce]; len(hmacNonce) > 0 {
+    repBuff.WriteString("nonce:"+hmacNonce[0]+"\n")
+  } else {
+    repBuff.WriteString("nonce:\n")
+  }
+
+  // request params
+  params := canonicalParams(req.Form)
+  for _, v := range params {
+    repBuff.WriteString(v+":"+req.Form[v][0]+"\n")
+  }
 
   rep = repBuff.String()
   return rep, nil
+}
+
+func canonicalParams(m url.Values) []string {
+    mk := make([]string, len(m))
+    i := 0
+    for k, _ := range m {
+        mk[i] = k
+        i++
+    }
+    sort.Strings(mk)
+    return mk
 }
